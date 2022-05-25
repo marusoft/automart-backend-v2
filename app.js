b58b0eb9568e5b397961a5a590c4e52029230326
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-05-25 14:26:59 +0100

    fix typo in app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
-    res.headers("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
     return res.status(200).json({});
   }
   next();
